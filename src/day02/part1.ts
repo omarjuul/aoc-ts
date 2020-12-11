eb0481d8766238c9dc095a8c36ca39bbcc8262db
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-11 23:51:33 +0100

    day 2 complete

--- src/day02/part1.ts
+++ src/day02/part1.ts
@@ -1,5 +1,36 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: mapLine } }).filter(x => x.pw);
 
-// TODO: Complete Part 1
+function mapLine(line: string) {
+    const splitLine = line.split(': ');
+    const rule = parseRule(splitLine[0]);
+    return { rule: rule, pw: splitLine[1] };
+}
+
+function parseRule(ruleStr: string): Rule {
+    const x = ruleStr.split(' ');
+    const minmax = x[0].split('-');
+    return { min: +minmax[0], max: +minmax[1], character: x[1] };
+}
+
+export default input.map(isValidPassword).reduce((acc, valid) => acc + +valid, 0);
+
+function isValidPassword(x: PwDef): boolean {
+    const matches = x.pw.match(new RegExp(x.rule.character, 'g'))?.length ?? 0;
+    const isValid = matches <= x.rule.max && matches >= x.rule.min;
+    // console.log(x, matches);
+    // console.log(isValid ? 'valid' : 'invalid');
+    return isValid;
+}
+
+interface PwDef {
+    rule: Rule;
+    pw: string;
+}
+
+interface Rule {
+    min: number;
+    max: number;
+    character: string;
+}
--- src/day02/part2.ts
+++ src/day02/part2.ts
@@ -1,5 +1,33 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: mapLine } }).filter(x => x.pw);
 
-// TODO: Complete Part 2
+function mapLine(line: string) {
+    const splitLine = line.split(': ');
+    const rule = parseRule(splitLine[0]);
+    return { rule: rule, pw: splitLine[1] };
+}
+
+function parseRule(ruleStr: string): Rule {
+    const x = ruleStr.split(' ');
+    const minmax = x[0].split('-');
+    return { min: +minmax[0], max: +minmax[1], character: x[1] };
+}
+
+export default input.map(isValidPassword).reduce((acc, valid) => acc + +valid, 0);
+
+function isValidPassword(x: PwDef): boolean {
+    return (x.pw[x.rule.min - 1] === x.rule.character)
+        !== (x.pw[x.rule.max - 1] === x.rule.character);
+}
+
+interface PwDef {
+    rule: Rule;
+    pw: string;
+}
+
+interface Rule {
+    min: number;
+    max: number;
+    character: string;
+}
