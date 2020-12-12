2d16074bb3be2b709e7ad4e1b11085ad5b3927f3
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-13 00:05:34 +0100

    day 8 complete

--- src/day08/part1.ts
+++ src/day08/part1.ts
@@ -1,5 +1,42 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false } }).filter(s => s);
+const instructions = input.map(parseInstr);
 
-// TODO: Complete Part 1
+interface Instr {
+    op: 'acc' | 'jmp' | 'nop';
+    arg: number;
+}
+
+console.log(instructions);
+const visited = new Set<number>();
+let idx = 0;
+let accum = 0;
+while (!visited.has(idx)) {
+    visited.add(idx);
+    const instr = instructions[idx];
+    switch (instr.op) {
+        case 'acc':
+            accum += instr.arg;
+            idx++;
+            break;
+        case 'jmp':
+            idx += instr.arg;
+            break;
+        case 'nop':
+            idx++;
+            break;
+    }
+    console.log(idx);
+}
+export default accum;
+
+export function parseInstr(str: string): Instr {
+    const s = str.split(' ');
+    const op = s[0];
+    const arg = +s[1];
+    if (op !== 'acc' && op !== 'jmp' && op !== 'nop')
+        throw new Error(`Unknown operation '${op}'`);
+
+    return { op, arg };
+}
--- src/day08/part2.ts
+++ src/day08/part2.ts
@@ -1,5 +1,50 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false } }).filter(s => s);
+const instructions = input.map(parseInstr);
 
-// TODO: Complete Part 2
+interface Instr {
+    op: 'acc' | 'jmp' | 'nop';
+    arg: number;
+}
+
+const accum = (() => {
+    for (let i = 0; i < instructions.length; i++) {
+        const visited = new Set<number>();
+        let idx = 0;
+        let accum = 0;
+        while (!visited.has(idx)) {
+            visited.add(idx);
+            if (idx >= instructions.length)
+                return accum;
+            const instr = instructions[idx];
+            let op = instr.op;
+            if (idx == i && op !== 'acc')
+                op = op === 'nop' ? 'jmp' : 'nop';
+            switch (op) {
+                case 'acc':
+                    accum += instr.arg;
+                    idx++;
+                    break;
+                case 'jmp':
+                    idx += instr.arg;
+                    break;
+                case 'nop':
+                    idx++;
+                    break;
+            }
+        }
+    }
+})();
+
+export default accum;
+
+export function parseInstr(str: string): Instr {
+    const s = str.split(' ');
+    const op = s[0];
+    const arg = +s[1];
+    if (op !== 'acc' && op !== 'jmp' && op !== 'nop')
+        throw new Error(`Unknown operation '${op}'`);
+
+    return { op, arg };
+}
