00959d53408cc379e2e135fb059a4011397fa01f
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-13 00:40:06 +0100

    day 9 complete

--- src/day09/part1.ts
+++ src/day09/part1.ts
@@ -1,5 +1,19 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+export const input = parseInput();
+const PREV_WINDOW_SIZE = 25;
 
-// TODO: Complete Part 1
+function findFirstNoSum(): number | undefined {
+    blah: for (let i = PREV_WINDOW_SIZE; i < input.length; i++) {
+        const current = input[i];
+        for (let a = i - PREV_WINDOW_SIZE; a < i; a++) {
+            for (let b = a + 1; b < i; b++) {
+                if (input[a] + input[b] === current)
+                    continue blah;
+            }
+        }
+        return current;
+    }
+}
+
+export default findFirstNoSum();
--- src/day09/part2.ts
+++ src/day09/part2.ts
@@ -1,5 +1,31 @@
-import { parseInput } from '../util';
+import { setupMaster } from 'cluster';
+import { default as step1, input } from './part1';
 
-const input = parseInput();
+if (!step1)
+    throw new Error("WHAAAA! PANIC!");
 
-// TODO: Complete Part 2
+export default (() => {
+    for (let start = 0; start < input.length; start++) {
+        for (let end = start + 1; sumFromTo(start, end) <= step1; end++) {
+            if (sumFromTo(start, end) === step1)
+                return getMinMaxSum(start, end);
+        }
+    }
+})();
+
+function sumFromTo(start: number, end: number): number {
+    return input.slice(start, end).reduce((a, b) => a + b);
+}
+
+function getMinMaxSum(start: number, end: number): number {
+    const subArray = input.slice(start, end);
+    return subArray.reduce(min) + subArray.reduce(max);
+}
+
+function min(a: number, b: number) {
+    return Math.min(a, b);
+}
+
+function max(a: number, b: number) {
+    return Math.max(a, b);
+}
