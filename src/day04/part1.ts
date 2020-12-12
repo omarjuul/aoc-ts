0a129d94d4cf46e33b93da4a8be33cdefd27cbd1
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-12 12:54:56 +0100

    day 4 complete

--- src/day04/part1.ts
+++ src/day04/part1.ts
@@ -1,5 +1,29 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false, delimiter: "\r\n\r\n" } });
 
-// TODO: Complete Part 1
+const requiredFields = [
+    'byr',
+    'iyr',
+    'eyr',
+    'hgt',
+    'hcl',
+    'ecl',
+    'pid',
+    //'cid',
+]
+
+export default input
+    .map(getFields)
+    .map(hasRequiredFields)
+    .reduce((acc, cur) => acc += +cur, 0);
+
+function getFields(str: string): string[] {
+    const fieldsWithValues = str.split('\r\n').flatMap(l => l.split(' '));
+    return fieldsWithValues.map(fwv => fwv.split(':')[0])
+}
+
+function hasRequiredFields(fields: string[]) {
+    const retVal = requiredFields.every(f => fields.includes(f));
+    return retVal;
+}
--- src/day04/part2.ts
+++ src/day04/part2.ts
@@ -1,5 +1,52 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false, delimiter: "\r\n\r\n" } });
 
-// TODO: Complete Part 2
+const requiredFields = [
+    { f: 'byr', validate: (s: string) => +s >= 1920 && +s <= 2002 },
+    { f: 'iyr', validate: (s: string) => +s >= 2010 && +s <= 2020 },
+    { f: 'eyr', validate: (s: string) => +s >= 2020 && +s <= 2030 },
+    { f: 'hgt', validate: valHgt },
+    { f: 'hcl', validate: (s: string) => s.search(/^\#[0-9a-f]{6}$/) === 0 },
+    { f: 'ecl', validate: valEcl },
+    { f: 'pid', validate: (s: string) => s.search(/^[0-9]{9}$/) === 0 },
+    //'cid',
+]
+
+export default input
+    .map(getFields)
+    .map(hasValidFields)
+    .reduce((acc, cur) => acc += +cur, 0);
+
+function getFields(str: string): string[][] {
+    const fieldsWithValues = str.split('\r\n').flatMap(l => l.split(' '));
+    return fieldsWithValues.map(fwv => fwv.split(':'))
+}
+
+function hasValidFields(fields: string[][]) {
+    const retVal = requiredFields.every(rf => {
+        const field = fields.find(f => f[0] === rf.f);
+        if (!field)
+            return false;
+        const retVal = rf.validate(field[1]);
+        // if (!retVal)
+        //     console.log(retVal, rf.f, field);
+        return retVal;
+    });
+    return retVal;
+}
+
+function valHgt(s: string) {
+    const val = +s.substring(0, s.length - 2);
+    if (s.endsWith('cm'))
+        return val >= 150 && val <= 193;
+    else if (s.endsWith('in'))
+        return val >= 59 && val <= 76
+    else
+        return false;
+}
+
+function valEcl(s: string) {
+    const validHairColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+    return validHairColors.includes(s);
+}
