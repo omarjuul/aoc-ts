04fa8906aea51b0d22519a650c622abde14fe10f
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-12 21:21:56 +0100

    day 6 complete

--- src/day06/part1.ts
+++ src/day06/part1.ts
@@ -1,5 +1,19 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false, delimiter: '\r\n\r\n' } })
+    .map(group => group.split('\r\n'));
 
-// TODO: Complete Part 1
+function determineGroupYesCount(group: string[]): number {
+    const yesQuestions = new Set();
+    const flatGroup = group.map(str => [...str]).flat();
+    flatGroup.forEach(q => yesQuestions.add(q));
+
+    return yesQuestions.size;
+}
+
+const perGroup = input.map(determineGroupYesCount);
+export default perGroup.reduce(sum);
+
+function sum(a: number, b: number) {
+    return a + b;
+}
--- src/day06/part2.ts
+++ src/day06/part2.ts
@@ -1,5 +1,22 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false, delimiter: '\r\n\r\n' } })
+    .map(group => group.split('\r\n').filter(str => str));
 
-// TODO: Complete Part 2
+function determineGroupYesCount(group: string[]): number {
+    const groupAnswers = group.map(str => [...str]);
+    let allHaveAnswer = groupAnswers[0];
+    for (let idx = 1; idx < group.length; idx++) {
+        const e = groupAnswers[idx];
+        allHaveAnswer = allHaveAnswer.filter(a => e.includes(a));
+    }
+    console.log(allHaveAnswer, allHaveAnswer.length);
+    return allHaveAnswer.length;
+}
+
+const perGroup = input.map(determineGroupYesCount);
+export default perGroup.reduce(sum);
+
+function sum(a: number, b: number) {
+    return a + b;
+}
