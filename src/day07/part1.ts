195cdc61109743faca3c3f60a5aeed9bd64fb10f
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-12 22:45:38 +0100

    day 7 complete

--- src/day07/part1.ts
+++ src/day07/part1.ts
@@ -1,5 +1,33 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false } }).filter(s => s);
+const rules = input.map(parseRules);
 
-// TODO: Complete Part 1
+console.log(rules);
+let oldCount = 0;
+const bagsThatWork = new Set<{ bagId: string, childBags: string[] }>();
+rules.filter(r => r.childBags.includes('shiny gold')).forEach(r => bagsThatWork.add(r));
+while (bagsThatWork.size > oldCount) {
+    // not very efficient,
+    // recursion would probably be much better than iteration here
+    // but whatever
+    oldCount = bagsThatWork.size;
+    bagsThatWork
+        .forEach(b => rules
+            .filter(r => r.childBags.includes(b.bagId))
+            .forEach(r => bagsThatWork.add(r)));
+    console.log(bagsThatWork);
+}
+
+export default bagsThatWork.size;
+
+function parseRules(ruleStr: string) {
+    const t = ruleStr.split(' contain ');
+    const bagId = t[0].split(' bag')[0];
+    const childBags = t[1]
+        .split(', ')
+        .map(s => s
+            .substring(s.indexOf(' ') + 1)
+            .split(' bag')[0]);
+    return { bagId, childBags };
+}
--- src/day07/part2.ts
+++ src/day07/part2.ts
@@ -1,5 +1,40 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: false } }).filter(s => s);
+const rules = input.map(parseRule);
 
-// TODO: Complete Part 2
+interface BagRule {
+    bagId: string;
+    childBags: { bagId: string, amount: number }[];
+}
+
+export default calculateNrOfBagsById('shiny gold') - 1;
+
+function calculateNrOfBagsById(bagId: string): number {
+    const bag = rules.find(r => r.bagId === bagId);
+    if (!bag)
+        return 0;
+    return calculateNrOfBags(bag);
+}
+
+function calculateNrOfBags(bag: BagRule): number {
+    return 1 + bag.childBags
+        .map(cb => cb.amount * calculateNrOfBagsById(cb.bagId))
+        .reduce((a, b) => a + b);
+}
+
+function parseRule(ruleStr: string): BagRule {
+    const t = ruleStr.split(' contain ');
+    const bagId = t[0].split(' bag')[0];
+    const childBags = t[1]
+        .split(', ')
+        .map(s => {
+            const matches = s.match(/^(\d+) (\w+ \w+) bag/);
+            if (!matches)
+                return { bagId: '', amount: 0 };
+
+            return { bagId: matches[2], amount: +matches[1] };
+        });
+    // console.log(childBags);
+    return { bagId, childBags };
+}
