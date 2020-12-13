4a451019d39dc86a56423eafb0a0988bac3f4eef
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-13 22:15:57 +0100

    day 12 part 1 compete

--- src/day12/part1.ts
+++ src/day12/part1.ts
@@ -1,5 +1,56 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: parseInstr } }).filter(x => null !== x);
 
-// TODO: Complete Part 1
+const dest = input.reduce((pos: Position, d) => d!(pos), { x: 0, y: 0, face: 'E' });
+console.log(dest.x, dest.y);
+
+export default Math.abs(dest.x) + Math.abs(dest.y);
+
+function parseInstr(str: string) {
+    console.log(str);
+    if (!str)
+        return null;
+    const matches = str.match(/^(\w)(\d+)$/)!;
+    const direction = parseDirection(matches[1], +matches[2]);
+    return direction;
+}
+
+interface Position {
+    x: number;
+    y: number;
+    face: 'N' | 'S' | 'E' | 'W';
+}
+
+function parseDirection(direction: string, value: number): ((p: Position) => Position) {
+    switch (direction) {
+        case 'N':
+            return (p: Position) => ({ y: p.y + value, x: p.x, face: p.face });
+        case 'S':
+            return (p: Position) => ({ y: p.y - value, x: p.x, face: p.face });
+        case 'E':
+            return (p: Position) => ({ x: p.x + value, y: p.y, face: p.face });
+        case 'W':
+            return (p: Position) => ({ x: p.x - value, y: p.y, face: p.face });
+
+        case 'L':
+            return (p: Position) => ({ x: p.x, y: p.y, face: getNewFace(p.face, -value) });
+        case 'R':
+            return (p: Position) => ({ x: p.x, y: p.y, face: getNewFace(p.face, value) });
+
+        case 'F':
+            return (p: Position) => {
+                return parseDirection(p.face, value)(p)
+            };
+
+        default:
+            throw new Error(`unknown direction ${direction}`);
+    }
+}
+
+function getNewFace(face: 'N' | 'S' | 'E' | 'W', clockwiseDegrees: number): 'N' | 'S' | 'E' | 'W' {
+    const steps = clockwiseDegrees / 90;
+    const faces: ('N' | 'S' | 'E' | 'W')[] = ['N', 'E', 'S', 'W'];
+    const retVal = faces[(faces.indexOf(face) + steps + 4) % 4];
+    return retVal;
+}
