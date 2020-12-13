34a0e5720bb15d5199a4e95065e0b1799ee0606f
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-13 11:59:24 +0100

    day 10 complete

--- src/day10/part1.ts
+++ src/day10/part1.ts
@@ -2,4 +2,20 @@ import { parseInput } from '../util';
 
 const input = parseInput();
 
-// TODO: Complete Part 1
+const sortedRatings = input.sort((a, b) => a - b);
+console.log(sortedRatings);
+
+let jumpsOf1 = 0;
+let jumpsOf3 = 0;
+
+for (let i = 0; i < sortedRatings.length - 1; i++) {
+    const jump = sortedRatings[i + 1] - sortedRatings[i];
+    if (jump === 3)
+        jumpsOf3++;
+    else if (jump === 1)
+        jumpsOf1++;
+    else if (jump !== 2)
+        throw new Error("jump too big!");
+}
+
+export default jumpsOf1 * (jumpsOf3 + 1);
--- src/day10/part2.ts
+++ src/day10/part2.ts
@@ -1,5 +1,57 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput().filter(x => x);
+input.push(0);
 
-// TODO: Complete Part 2
+const sortedRatings = input.sort((a, b) => a - b);
+console.log(sortedRatings);
+
+const jumps: number[] = [];
+
+for (let i = 0; i < sortedRatings.length - 1; i++) {
+    const jump = sortedRatings[i + 1] - sortedRatings[i];
+    jumps.push(jump);
+}
+
+// only jumps of 1 and 3, so we can count the streaks of 1-jumps
+// as jumps of 3 in the arrangement with all adapters must be in every valid arrangement
+
+const streakLengths: number[] = [];
+let currentStreak = 0;
+for (let i = 0; i < jumps.length; i++) {
+    const jump = jumps[i];
+    if (jump === 1) {
+        currentStreak++;
+    }
+    else if (jump === 3) {
+        streakLengths.push(currentStreak);
+        currentStreak = 0;
+    }
+}
+if (currentStreak > 0)
+    streakLengths.push(currentStreak);
+
+console.log(jumps, streakLengths);
+
+export default streakLengths.map(calculateVariations).reduce(product);
+
+function calculateVariations(streakLength: number): number {
+    // could cache values if necessary, but not needed for now
+    switch (streakLength) {
+        case 0:
+        case 1:
+            return 1;
+        case 2:
+            return 2;
+        case 3:
+            return 4;
+        default:
+            return calculateVariations(streakLength - 3) +
+                calculateVariations(streakLength - 2) +
+                calculateVariations(streakLength - 1);
+    }
+}
+
+function product(a: number, b: number): number {
+    return a * b;
+}
