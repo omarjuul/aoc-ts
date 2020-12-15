f0443ab18c6c6af58f67895993cea23a2dee5d85
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-15 16:18:58 +0100

    day 15: use proper Map

--- src/day15/part2_bruteforce.ts
+++ src/day15/part2_bruteforce.ts
@@ -2,17 +2,17 @@ import { parseInput } from '../util';
 
 const input = parseInput({ split: { delimiter: ',' } })
 const n = 30_000_000
-const lastIdxOf: { [x: number]: number } = {}
-input.slice(0, -1).map((x, idx) => ({ x, idx })).forEach(i => lastIdxOf[i.x] = i.idx)
+const lastIdxOf = new Map<number, number>()
+input.slice(0, -1).map((x, idx) => ({ x, idx })).forEach(i => lastIdxOf.set(i.x, i.idx))
 
 for (let i = input.length; i < n; i++) {
-    const lastNr = input[i - 1]
-    const lastIdxBefore = lastIdxOf[lastNr]
-    input[i] = lastIdxBefore === undefined
+    const prev = input[i - 1]
+    const lastIdx = lastIdxOf.get(prev)
+    input[i] = lastIdx === undefined
         ? 0
-        : i - 1 - lastIdxBefore
+        : (i - 1) - lastIdx
 
-    lastIdxOf[input[i - 1]] = i - 1
+    lastIdxOf.set(input[i - 1], i - 1)
 
     if (i % 30000 === 0) console.log(`${Math.round(i / n * 10000) / 100}%`)
 }
