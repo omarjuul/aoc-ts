43c068fd1653dddb8b4438f92d4c256516550221
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-17 23:59:11 +0100

    use destructuring with split (day 2)

--- src/day02/part1.ts
+++ src/day02/part1.ts
@@ -2,23 +2,23 @@ import { parseInput } from '../util';
 
 const input = parseInput({ split: { mapper: mapLine } }).filter(x => x.pw);
 
-function mapLine(line: string) {
-    const splitLine = line.split(': ');
-    const rule = parseRule(splitLine[0]);
-    return { rule: rule, pw: splitLine[1] };
+function mapLine(line: string): PwDef {
+    const [ruleStr, pw] = line.split(': ');
+    const rule = parseRule(ruleStr);
+    return { rule, pw };
 }
 
 function parseRule(ruleStr: string): Rule {
-    const x = ruleStr.split(' ');
-    const minmax = x[0].split('-');
-    return { min: +minmax[0], max: +minmax[1], character: x[1] };
+    const [minmax, char] = ruleStr.split(' ');
+    const [min, max] = minmax.split('-');
+    return { min: +min, max: +max, character: char };
 }
 
 export default input.map(isValidPassword).reduce((acc, valid) => acc + +valid, 0);
 
 function isValidPassword(x: PwDef): boolean {
     const matches = x.pw.match(new RegExp(x.rule.character, 'g'))?.length ?? 0;
-    const isValid = matches <= x.rule.max && matches >= x.rule.min;
+    const isValid = x.rule.min <= matches && matches <= x.rule.max;
     // console.log(x, matches);
     // console.log(isValid ? 'valid' : 'invalid');
     return isValid;
