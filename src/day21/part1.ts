2e6de99b29f4099f2fa8f84004d6c815a5d17219
Camiel Egbers <camiel.egbers@gmail.com>
2020-12-21 11:23:46 +0100

    day 21 complete

--- src/day21/part1.ts
+++ src/day21/part1.ts
@@ -1,5 +1,50 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: parseFood } });
+// console.log(input);
 
-// TODO: Complete Part 1
+export const allergenMap = new Map<string, Set<string>>()
+for (const { alrg, ingr } of input.flat()) {
+    if (!allergenMap.has(alrg)) {
+        allergenMap.set(alrg, new Set(ingr.values()))
+    } else {
+        const prevSet = allergenMap.get(alrg)!
+        allergenMap.set(alrg, intersectionOf(prevSet, ingr))
+    }
+}
+// console.log(allergenMap);
+
+const dangerous = new Set<string>()
+for (const ingrWithAlrg of allergenMap.values()) {
+    for (const ingr of ingrWithAlrg) {
+        dangerous.add(ingr)
+    }
+}
+
+const allIngr = input.flatMap(i => Array.from(i[0].ingr.values()))
+const safe = allIngr.filter(ingr => !dangerous.has(ingr))
+// console.log(allIngr, safe)
+export default safe.length
+
+function parseFood(line: string) {
+    const [ingredients, allergens] = line.replace(')', '').split(' (contains ')
+    const ingr = ingredients.split(' ')
+    return allergens.split(', ').map(a => ({ alrg: a, ingr: new Set(ingr) }))
+}
+
+function intersectionOf<T>(left: Set<T>, right: Set<T>): Set<T> {
+    const intersection = new Set<T>()
+    for (const leftEl of left) {
+        if (right.has(leftEl))
+            intersection.add(leftEl)
+    }
+    return intersection
+}
+
+function unionOf<T>(left: Set<T>, right: Set<T>): Set<T> {
+    const union = new Set<T>(left.values())
+    for (const rightEl of right) {
+        union.add(rightEl)
+    }
+    return union
+}
--- src/day21/part2.ts
+++ src/day21/part2.ts
@@ -1,5 +1,19 @@
-import { parseInput } from '../util';
+import { allergenMap } from './part1';
 
-const input = parseInput();
+const allergensWithIngr = spliceAndExtractDecided().sort((a, b) => a.allergen > b.allergen ? 1 : -1)
+console.log(allergensWithIngr);
+export default allergensWithIngr.map(x => x.ingredient).join(',')
 
-// TODO: Complete Part 2
+function spliceAndExtractDecided(): { allergen: string, ingredient: string }[] {
+    const decided = Array.from(allergenMap.entries())
+        .filter(e => e[1].size === 1)
+        .map(e => ({ allergen: e[0], ingredient: e[1].values().next().value }))
+    if (decided.length === 0) return decided
+
+    decided.forEach(d => {
+        for (const set of allergenMap.values()) {
+            set.delete(d.ingredient)
+        }
+    })
+    return decided.concat(spliceAndExtractDecided())
+}
